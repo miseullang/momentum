b494900095fd06d7dc3211016b45831f4793b5db
슬 <seul@I-got-no-energy.local>
2024-09-11 17:29:08 +0900

    sparkles:feat: 요소 정렬 / to-do 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,22 @@ const todoForm = document.querySelector('#todo-form');
 const todoInput = todoForm.querySelector('input');
 const todoList = document.querySelector('#todo-list');
 
+function deleteTodo(evnet) {
+    const li = evnet.target.parentElement;
+    li.remove();
+}
+
 // 받아온 값을 ul 내부로 넣어주는 함수
 function createTodo(newTodo) {
     const li = document.createElement('li');
     const span = document.createElement('span');
+    span.innerText = newTodo;
+    const button = document.createElement('button');
+    button.innerText = '❌';
+    button.addEventListener('click', deleteTodo)
 
     li.appendChild(span);
     li.appendChild(button);
-
-    span.innerText = newTodo;
     todoList.appendChild(li);
 }
 
