24b23bf87445c7cdc62cdcee2b357743a80dddbb
슬 <seul@I-got-no-energy.local>
2024-09-11 22:11:09 +0900

    :sparkles:feat: 날씨 API 추가

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = '669d3a333221c25db689662ed04f33e2';
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log('당신의 위치는', lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+     .then(response => response.json())
+     .then(data => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        weather.innerText = `${data.weather[0].main} / ${(data.main.temp).toFixed(1)}˚`
+        city.innerText = data.name
+     })
+
+
 }
 
 function onGeoError() {
