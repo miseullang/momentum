0a0d6e5840faec54351599baaf0b2bc990081487
miseullang <155376060+miseullang@users.noreply.github.com>
2024-09-19 17:44:16 +0900

    :sparkles:feat: 음악 재생,일시정지 기능 추가/새로고침/밈 메이커/리드미 추가

---
+++ clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector('#clock');
+
+function getClock() {
+    const date = new Date();
+    const hour = date.getHours() > 12 
+    ? `PM ${('00' + (date.getHours() - 12)).slice(-2)}` 
+    : `AM ${('00' + date.getHours()).slice(-2)}`;
+    const minute = ('00' + date.getMinutes()).slice(-2);
+    const second = ('00' + date.getSeconds()).slice(-2);
+    clock.innerText = `${hour}:${minute}:${second}`;
+    // clock.innerText = new Date().toLocaleTimeString();
+    // => 이렇게 하면 한방에 해결 가능...ㅎ 다만 PM 02시가 아니라 오후 2시로 표시됨
+}
+
+getClock();
+setInterval(getClock,1000);
